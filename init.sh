8d2d7f3f324c17b813d646e9f77cc82b3089e6e5
lyon <645275593@qq.com>
2021-08-03 22:53:44 +0800

    cancel docker

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ rm src
 echo link ../src to $PWD
 ln -s ../src $PWD/src
 
-echo copy setup/vscode to ../.vscode
-cp setup/vscode/c.json.code-snippets ../.vscode/
-cd docker && sh build.sh && cd ..
+# echo copy setup/vscode to ../.vscode
+# cp setup/vscode/c.json.code-snippets ../.vscode/
+# cd docker && sh build.sh && cd ..
